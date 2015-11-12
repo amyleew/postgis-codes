b3452310d3c31e936eadba9350cfcbf02e7b8d86
amyleewalton <amylee@mapbox.com>
2015-11-12 14:07:43 -0500

    fix typo

--- postgis-uploads/tippe-uploads.sh
+++ postgis-uploads/tippe-uploads.sh
@@ -3,13 +3,9 @@
 
 ## Install mapbox-upload API (if you have ignore)
 
-# npm install --global mapbox-upload
-# npm install --save mapbox-upload
-# npm install mapbox-upload
-
-
+npm install --save mapbox-upload
 
 ## Third: upload .mbtiles with Mapbox Uploads API
 
-node /Users/amyleewalton/amyleew/_repo/postgis-codes/postgis-uploads/upload.js minor_islands.mbtiles mslee $MapboxAccessToken minorislands
-node /Users/amyleewalton/amyleew/_repo/postgis-codes/postgis-uploads/upload.js minor_islands_coastline.mbtiles mslee $MapboxAccessToken minorislandscoast
+node upload.js minor_islands.mbtiles mslee $MapboxAccessToken minorislands
+node upload.js minor_islands_coastline.mbtiles mslee $MapboxAccessToken minorislandscoast
